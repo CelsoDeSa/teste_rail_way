9e352cefc9fe55dfd087c070e874e16000703665
Celso de Sá <celso.sa.lopes@gmail.com>
2023-01-28 15:55:59 -0300

    Add host permission

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,9 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts = [
+    "testerailway-production.up.railway.app"
+  ]
 
   # Code is not reloaded between requests.
   config.cache_classes = true
